a016b206275bd908dfcece249a7a857d211927e7
valerysand <kolomietsval@gmail.com>
2022-11-13 10:16:53 +0200

    changes

--- src/logic/auth-facebook.logic.ts
+++ src/logic/auth-facebook.logic.ts
@@ -59,7 +59,9 @@ export async function getAcсessToken(): Promise<string> {
     // Check if token is valid
     async function checkTokenExpiration(): Promise<boolean> {
         try {
+            // Get the token from mongoDB
             const token = await getStoredToken();
+            // Get the token info
             const response = await axios.get(
                 `https://graph.facebook.com/debug_token?input_token=${token}&access_token=${token}`
             );
--- src/logic/facebook-logic.ts
+++ src/logic/facebook-logic.ts
@@ -2,33 +2,59 @@ import axios from "axios";
 import { FacebookStreamModel, IFacebookStreamModel } from "../model/stream-models/facebook-stream.model";
 import { getAcсessToken } from "./auth-facebook.logic";
 
-// Get my user Data
-export async function getMyUserData(): Promise<any> {
-    const access_token = await getAcсessToken();
-    const response = await axios.get(
-        `https://graph.facebook.com/v15.0/me?fields=id%2Cname%2Cemail&access_token=${access_token}`
-    );
-    return response.data;
-}
 // Get my live videos
 export async function getMyLiveVideos(): Promise<any> {
     const access_token = await getAcсessToken();
-    const response = await axios.get(
-        `https://graph.facebook.com/v15.0/me?fields=id%2Cname%2Clive_videos%7Bid%2Ctitle%2Cdescription%2Clive_views%2Ccomments%7Bmessage%7D%2Creactions%7D&access_token=${access_token}`
-    );
-    const liveStream = response.data.live_videos.data[0];
-    // Save the data to the DB
-    const data: IFacebookStreamModel = new FacebookStreamModel({
-        streamId: liveStream.id,
-        title: liveStream.title,
-        description: liveStream.description,
-        reactions: liveStream.reactions.data.length,
-        views: liveStream.live_views,
-        comments: liveStream.comments.data.map((comment: any) => comment.message),
-    });
-
-    await data.save();
-    console.log('Facebook live stream data saved in mongoDB');
-    return data;
+    // Check if token is valid
+    if (access_token) {
+        // Get the live videos
+        const response = await axios.get(
+            `https://graph.facebook.com/v15.0/me?fields=id%2Cname%2Clive_videos%7Bid%2Ctitle%2Cdescription%2Clive_views%2Ccomments%7Bmessage%7D%2Creactions%7D&access_token=${access_token}`
+        );
+        // 
+        const liveStream = response.data.live_videos.data[0];
+        // Check if the stream is live
+        if (liveStream) {
+            // Check if the stream is already in the database
+            const isStreamInDB = await FacebookStreamModel.findOne({ streamId: liveStream.id });
+            if (!isStreamInDB) {
+                // Save the stream to the database
+                const saveStream: IFacebookStreamModel = new FacebookStreamModel({
+                    streamId: liveStream.id,
+                    title: liveStream.title,
+                    description: liveStream.description,
+                    reactions: liveStream.reactions.saveStream.length,
+                    views: liveStream.live_views,
+                    comments: liveStream.comments.data.map((comment: any) => comment.message),
+                });
+                // Save the stream
+                await saveStream.save();
+                console.log('Facebook live stream data saved in mongoDB');
+                return saveStream;
+            }
+            else {
+                // Update the stream in the database
+                await FacebookStreamModel.updateOne({ streamId: liveStream.id }, {
+                    $set: {
+                        title: liveStream.title,
+                        description: liveStream.description,
+                        reactions: liveStream.reactions.saveStream.length,
+                        views: liveStream.live_views,
+                        comments: liveStream.comments.data.map((comment: any) => comment.message),
+                    }
+                });
+                console.log('Facebook live stream data updated in mongoDB');
+                return isStreamInDB;
+            }
+        }
+        else {
+            console.log('No live stream found on Facebook ');
+            return null;
+        }
+    }
+    else {
+        console.log('No access token for Facebook');
+        return null;
+    }
 }
 
